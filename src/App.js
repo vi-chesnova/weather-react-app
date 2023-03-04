c9e96f69a06157a1837203e10fdcf0d42ed2dc7d
vi-chesnova <v.chesnova@icloud.com>
2023-03-04 22:23:13 +0100

    Created HTML framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Weather from "./Weather";
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by Victoria Chesnova and is{" "}
           <a
