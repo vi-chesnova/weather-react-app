cb25afbbc680749ff3a8904ce1b01c1ba80fe032
vi-chesnova <v.chesnova@icloud.com>
2023-03-04 20:54:27 +0100

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Victoria Chesnova and is{" "}
+        <a
+          href="https://github.com/vi-chesnova/weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
