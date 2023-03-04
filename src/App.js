a1572f58a94d51e6d82af99f59acb2aa0ae4f2c7
vi-chesnova <v.chesnova@icloud.com>
2023-03-04 21:58:09 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Victoria Chesnova and is{" "}
-        <a
-          href="https://github.com/vi-chesnova/weather-react-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Victoria Chesnova and is{" "}
+          <a
+            href="https://github.com/vi-chesnova/weather-react-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
